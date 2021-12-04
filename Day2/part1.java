90404251dc19acba01cd8988dc98547939e56fde
ysidro <71098822+CarbonatedSoftDrink@users.noreply.github.com>
2021-12-03 20:21:00 -0500

    created java file for Day2/part1.

---
+++ Day2/part1.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.io.File;
+import java.util.Scanner;
+
+public class part1 {
+    public static int x = 0;
+    public static int y = 0;
+    public static int product = 0;
+
+    public static void main(String[] args) {
+        int i = 0;
+
+        System.out.println("x = " + x);
+        System.out.println("y = " + y);
+        System.out.println("Product of horizontal and vertical = " + product);
+    }
+}
