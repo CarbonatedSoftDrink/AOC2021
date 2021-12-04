925c317703b76cb3257cd85db9947d8e1cf1bdad
ysidro <71098822+CarbonatedSoftDrink@users.noreply.github.com>
2021-12-03 19:44:42 -0500

    made a function to detect the word's first letter and return a value based off of it.

--- Day2/part1.py
+++ Day2/part1.py
@@ -3,13 +3,21 @@ x = 0
 y = 0
 product = 0
 
+def extract(item):
+    x += 9
+    if (item[0] == 'f'):
+        return int(item[8])
+    elif (item[0] == 'd'):
+        return int(item[5])
+    elif (item[0] == 'u'):
+        return int(item[3])
+
 with open('Day2/input.txt') as f:
     directions = f.readlines()
 
 i = 0
 while i < len(directions):
-    if directions[0] == 'f':
-        x += int(directions[9])
+    x += extract(directions[i])
 
     i += 1
 
