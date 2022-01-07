1fb4cb112aaac4de4db378d35057d0eef58fcacd
ysidro <71098822+CarbonatedSoftDrink@users.noreply.github.com>
2022-01-07 16:41:05 -0500

    added arrays that hold winning combinations.

--- Day4/part1.java
+++ Day4/part1.java
@@ -6,10 +6,25 @@ import java.util.Scanner; // Import the Scanner class to read text files
 import java.util.ArrayList;
 public class part1 {
 
+    public static void determine(){
+        boolean bingo = false;
+
+    }
+
     public static void main(String[] args) {
         ArrayList<String> pulledNumbers = new ArrayList<String>();
         ArrayList<String> bingoCards = new ArrayList<String>();
         String[] bingoCard = new String[40];
+        int[] row1 = {0, 1, 2, 3, 4};
+        int[] row2 = {5, 6, 7, 8, 9};
+        int[] row3 = {10, 11, 12, 13, 14};
+        int[] row4 = {15, 16, 17, 18, 19};
+        int[] row5 = {20, 21, 22, 23, 24};
+        int[] col1 = {0, 5, 10, 15, 20};
+        int[] col2 = {1, 6, 11, 16, 21};
+        int[] col3 = {2, 7, 12, 17, 22};
+        int[] col4 = {3, 8, 13, 18, 23};
+        int[] col5 = {4, 9, 14, 19, 24};
         int cardIndex = -1;
         int cardMax = -1;
         int cardScore = -1;
