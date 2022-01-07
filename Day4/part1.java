b505f4eca9f359b4c465407eb65e656021b020f0
ysidro <71098822+CarbonatedSoftDrink@users.noreply.github.com>
2022-01-07 01:18:17 -0500

    continued work on part1 problem 4 in Java. added numbers of first bingo card.

--- Day4/part1.java
+++ Day4/part1.java
@@ -9,7 +9,10 @@ public class part1 {
     public static void main(String[] args) {
         ArrayList<String> pulledNumbers = new ArrayList<String>();
         ArrayList<String> bingoCards = new ArrayList<String>();
-        ArrayList<String> holdingList2 = new ArrayList<String>();
+        String[] bingoCard = new String[40];
+        int cardIndex = -1;
+        int cardMax = -1;
+        int cardScore = -1;
 
         try{
             File mine = new File("Day4/input.txt");
@@ -20,21 +23,30 @@ public class part1 {
             for (int i = 0; i < holdingList.length; i++){
                 pulledNumbers.add(holdingList[i]);
             }
+
             /*
-            while (reader.hasNext()){
-                data = reader.next();
-                pulledNumbers.add(data);
+            data = reader.next();
+            holdingList = data.split(" ");
+            for (int i = 0; i < holdingList.length; i++){
+                bingoCards.add(holdingList[i]);
             }
             */
+
+            //data = reader.next();
+            int i = 0;
+            while(i < 25){
+                data = reader.next();
+                bingoCards.add(data);
+                i++;
+            }
         }
         catch (FileNotFoundException e){
             System.out.println("Couldn't locate the file.");
         }
 
         System.out.println("Hello");
-        for (int i = 0; i < pulledNumbers.size(); i++){
-            System.out.println(pulledNumbers.get(i) + "/");
+        for (int i = 0; i < bingoCards.size(); i++){
+            System.out.println(bingoCards.get(i) + "/");
         }
-        System.out.println(pulledNumbers.get(6));
     }
 }
